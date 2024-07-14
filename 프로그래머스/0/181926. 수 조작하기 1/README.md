8a7da2215e4bab1d7996af84c35a6f576d5ec4db
Hyunseo Lee <153475125+0314pie@users.noreply.github.com>
2024-07-14 18:46:44 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.99 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/README.md
+++ 프로그래머스/0/181926. 수 조작하기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수 조작하기 1 - 181926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 10.93 ms
+메모리: 78.8 MB, 시간: 5.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 14일 18:28:46
+2024년 07월 14일 18:46:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, String control) {
+         for (int i = 0; i < control.length(); i++) {
+              char command = control.charAt(i);
+            switch (command) {
+                case 'w':
+                    n += 1;
+                    break;
+                case 's':
+                    n -= 1;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+                default:
+                    // handle unexpected character if needed
+                    break;
+            }
+        }
+        return n;
+    }
+    
+}
