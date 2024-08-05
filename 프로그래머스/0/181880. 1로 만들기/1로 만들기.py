5696e572631490ae9dbacb9cea385c650c160dde
Hyunseo Lee <153475125+0314pie@users.noreply.github.com>
2024-08-05 18:15:50 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    total_count = 0
+    
+    for num in num_list:
+        while num > 1:
+            if num % 2 == 0:
+                num //= 2
+            else:
+                num = (num - 1) // 2
+            total_count += 1
+    
+    return total_count
