6ac17e4334d8d4c2d42b0ee25aea50a021aa948b
Hyunseo Lee <153475125+0314pie@users.noreply.github.com>
2024-08-11 16:33:01 +0900

    [level 0] Title: 배열 만들기 6, Time: 295.34 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -4,13 +4,11 @@ def solution(arr):
     while i < len(arr):
         if not stk:
             stk.append(arr[i])
-            i += 1
         elif stk and stk[-1] == arr[i]:
             del stk[-1]
-            i += 1
         elif stk and stk[-1] != arr[i]:
             stk.append(arr[i])
-            i += 1
+        i += 1
     if not stk:
         return [-1]
     return stk
